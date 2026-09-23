d09a359858d155f72d9c40db230195b228a3723d
eallen <elise.allen@op.ac.nz>
2026-09-23 15:37:05 +1200

    Set up basic content for card demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import billm from '$lib/assets/images/groundhog-day.webp';
+</script>
+
+<header>
+    <h1>SvelteKit Components Demonstration</h1>
+    <p>For more information, view the <a href="https://svelte.dew/tutorial">Svelte tutorial</a></p>
+    <nav>
+        <a href="/">Home</a>
+    </nav>
+</header>
+
+<main>
+    <article class="card">
+        <img src="{billm}" alt="Phil Connors">
+        <section>
+            <h2>Phill Connors</h2>
+            <dl>
+                <dt>Favourite song:</dt>
+                <dd>I Got You Babe</dd>
+                <dt>Pet's name:</dt>
+                <dd>Punxsutawney Phil</dd>
+            </dl>
+        </section>
+    </article>
+</main>
+
+<footer>
+    <p>ID512 Fundamentals of Web Development &copy; 1993</p>
+</footer>
+
+<style>
+    
+</style>
