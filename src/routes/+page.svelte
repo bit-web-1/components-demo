37816ab40398dd4767c55bebc2991e51fafa96d7
eallen <elise.allen@op.ac.nz>
2026-09-23 15:55:59 +1200

    Add basic styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,5 +30,59 @@
 </footer>
 
 <style>
-    
+    :global(body) {
+        margin: 0;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+
+    header {
+        background-color: #f8f8f8;
+        padding: 1rem;
+        border-bottom: 1px solid #ccc;
+        nav {
+            border-top: 1px solid #ccc;
+            margin-top: 1rem;
+            padding-top: 1rem;
+        }
+    }
+
+    footer {
+        background-color: #f8f8f8;
+        padding: 1rem;
+        border-top: 1px solid #ccc;
+        text-align: center;
+    }
+
+    .card {
+        img {
+            width: 200px;
+            aspect-ratio: 1 / 1;
+            object-fit: cover;
+            object-position: center;
+            border-radius: 30px;
+            corner-shape: squircle;
+        }
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        padding: 1rem;
+        max-width: 600px;
+        margin: 1rem;
+    }
+
+    dl {
+        display: grid;
+        grid-template-columns: max-content 1fr;
+        gap: 0.5rem 1rem;
+    }
+
+    dt {
+        font-weight: bold;
+    }
+
+    dd {
+        margin: 0;
+    }
 </style>
